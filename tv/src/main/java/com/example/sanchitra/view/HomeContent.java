7b787a58fd0e66dff6180dc092256fff17b0abee
pypisan <sanjeev@pypisan.com>
2023-11-21 20:14:04 +0530

    view changed

---
+++ tv/src/main/java/com/example/sanchitra/view/HomeContent.java
@@ -0,0 +1,16 @@
+package com.example.sanchitra.view;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.leanback.app.RowsSupportFragment;
+
+public class HomeContent extends RowsSupportFragment {
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
--- tv/src/main/java/com/example/sanchitra/view/HomeView.java
+++ tv/src/main/java/com/example/sanchitra/view/HomeView.java
@@ -3,6 +3,7 @@ package com.example.sanchitra.view;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,5 +24,10 @@ public class HomeView extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        Fragment newContent = new HomeContent();
+        transaction.add(R.id.contentsFragment, newContent);
+        transaction.commit();
     }
 }
